fc41feeb91cc80056765c91b0a893102115804ce
Frosso Varsou <frossovarsou@gmail.com>
2025-01-15 19:23:03 +0200

    Refactor pgn_file_evaluation.py to enhance evaluation output formatting and adjust Stockfish parameters

--- Stockfish/pgn_file_evaluation.py
+++ Stockfish/pgn_file_evaluation.py
@@ -3,7 +3,10 @@ import chess.pgn
 from stockfish import Stockfish
 
 # Path to your Stockfish binary
-stockfish = Stockfish("/opt/homebrew/bin/stockfish", parameters={"Threads": 2, "Hash": 128})
+stockfish = Stockfish("/opt/homebrew/bin/stockfish", 
+                      parameters={"Threads": 2, 
+                                  "Hash": 1025,
+                                  })
 
 # Path to your PGN file
 username = input("Enter the username for latest PGN game: ")
@@ -24,15 +27,25 @@ for move in game.mainline_moves():
     board.push(move)  # Make the move on the board
     stockfish.set_fen_position(board.fen())  # Set the position in Stockfish
     evaluation = stockfish.get_evaluation()  # Get the evaluation from Stockfish
-    evaluations.append((move, evaluation))  # Save the move and evaluation
+    evaluations.append((move, evaluation))   # Save the move and evaluation
 
 # Print evaluations
-for move, eval_ in evaluations:
-    if eval_["type"] == "cp":
-        eval_score = eval_["value"]
-        print(f"Move: {move}, Evaluation: {eval_score} centipawns")
-    elif eval_["type"] == "mate":
-        print(f"Move: {move}, Evaluation: Mate in {eval_['value']} moves")
+print("\nEvaluations: (Positive is advantage white, negative is advantage black)")
+i = 1
+num_move = 1
+for move, eval in evaluations:
+
+    player = "WHITE" if i % 2 == 1 else "BLACK"
+    if i % 2 == 1 and i != 1:
+        num_move += 1
+    
+    i += 1
+
+    if eval["type"] == "cp":
+        eval_value = round(eval["value"] * 0.01, 2)  # e.g -1.50 like chess.com
+        print(f"{num_move}. {player} Move: {move}, Evaluation: {eval_value} centipawns")
+    elif eval["type"] == "mate":
+        print(f"{num_move}. {player} Move: {move}, Evaluation: Mate in {eval["value"]} moves")
 
 # Centipawn loss
 # This value can be used as an indicator of the quality of play. 
